035f8b2c4e7c3127d61d50317d93078d741bf56d
Mihai Ionescu <mihai.t.ionescu@gmail.com>
2023-03-25 14:48:00 -0400

    Added db configuration for the development mode

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 import os
 from flask import Flask, current_app, send_file
+from flask_sqlalchemy import SQLAlchemy
 
 from .api import api_bp
 from .client import client_bp
@@ -11,10 +12,17 @@ app.register_blueprint(api_bp)
 from .config import Config
 app.logger.info('>>> {}'.format(Config.FLASK_ENV))
 
+# check if running in test mode
+if os.environ.get('TESTING') == 'True':
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('TEST_DATABASE_URI')
+else:
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///flask_vuejs_dev_app.db'
+
+# initialize database connection
+db = SQLAlchemy(app)
+
 @app.route('/')
 def index_client():
     dist_dir = current_app.config['DIST_DIR']
     entry = os.path.join(dist_dir, 'index.html')
     return send_file(entry)
-
-
