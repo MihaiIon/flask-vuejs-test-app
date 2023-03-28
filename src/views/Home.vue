430fcfc7d2420a952d457718f5f2d67a1ae76878
Mihai Ionescu <mihai.t.ionescu@gmail.com>
2023-03-28 14:25:35 -0400

    Removed code from Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,21 +1,9 @@
 <template>
-  <div class="home">
-    <HelloWorld msg="Welcome to Your Vue.js + Flask App"/>
-  </div>
+  <div class="home"></div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
   name: 'home',
-  components: {
-    HelloWorld
-  }
 }
 </script>
-
-<style lang="scss">
-
-</style>
