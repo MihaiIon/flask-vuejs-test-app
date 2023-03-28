a4e3ed33a96ae282f21224c3bf2bfdaa6d31f4b8
Mihai Ionescu <mihai.t.ionescu@gmail.com>
2023-03-28 10:37:39 -0400

    Removed boilerplate Api.vue file

--- src/router.js
+++ src/router.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
-import Api from './views/Api.vue'
 
 Vue.use(Router)
 
@@ -12,10 +11,5 @@ export default new Router({
       name: 'home',
       component: Home
     },
-    {
-      path: '/api',
-      name: 'api',
-      component: Api
-    }
   ]
 })
