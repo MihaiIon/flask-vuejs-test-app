b8208f1195828efdceb592afdfc59806fb4fae73
Mihai Ionescu <mihai.t.ionescu@gmail.com>
2023-03-28 10:39:01 -0400

    Installed Vuetify

--- src/main.js
+++ src/main.js
@@ -2,13 +2,26 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
 
-import './filters'
+Vue.use(Vuetify)
+const vuetify = new Vuetify({
+  theme: {
+    dark: true,
+    themes: {
+      dark: {
+        primary: '#1976D2'
+      }
+    }
+  }
+})
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
+  vuetify,
   render: h => h(App)
 }).$mount('#app')
